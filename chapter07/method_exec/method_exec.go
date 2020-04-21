75eed7a173c27771f7c4e29b30b6fe4b61cae6bc
tom_glb <goodMorning_glb@atguigu.com>
2020-04-21 23:09:34 +0800

    add method exec demo

--- chapter07/method_exec/method_exec.go
+++ chapter07/method_exec/method_exec.go
@@ -12,6 +12,16 @@ func main() {
 
 	c.Area() //
 
+	var method MethodUtils
+	method.Print()
+	area := method.PrintArea(113, 13)
+	fmt.Println(area)
+	fmt.Println()
+	var cal Calcuator
+	cal.Num1 = 123.2
+	cal.Num2 = 143.2
+	res := cal.getRes('+')
+	fmt.Println(res)
 }
 
 // 编写一个结构体 Circle, 字段为radius
@@ -41,3 +51,51 @@ func (c *Circle) Areas() float64 {
 // 方法体
 // 返回值
 // }
+
+type MethodUtils struct {
+}
+
+func (mu MethodUtils) Print() {
+	for i := 1; i <= 10; i++ {
+		for j := 1; j <= 8; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+func (mu MethodUtils) PrintArea(len int64, width int64) int64 {
+
+	return len * width
+}
+
+type Calcuator struct {
+	Num1 float64
+	Num2 float64
+}
+
+func (calcuator *Calcuator) getRes(operator byte) float64 {
+	res := 0.0
+	switch operator {
+	case '+':
+		res = calcuator.Num1 + calcuator.Num2
+	case '-':
+		res = calcuator.Num1 - calcuator.Num2
+	case '*':
+		res = calcuator.Num1 * calcuator.Num2
+	case '/':
+		res = calcuator.Num1 / calcuator.Num2
+	default:
+		fmt.Println("运算符输入有误。。。")
+
+	}
+	return res
+
+}
+
+// 方法和函数的区别
+//1. 调用方式不一样
+//   函数调用方式：   函数名( 实参列表)
+//   方法调用方式：   变量.方法名( 实参列表)
+//2. 对于普通函数，接收者为值类型的时候，不能将指针类型的数据直接传递，反之亦然
+//3. 对于方法(如 struct的方法)， 接收者为值类型时，可以直接用指针类型变量调用方法，反之亦然
