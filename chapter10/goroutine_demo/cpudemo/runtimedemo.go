671fcbcb44ee8c81a857def9a5d257ef15423bc7
tom_glb <goodMorning_glb@atguigu.com>
2020-05-10 22:56:28 +0800

    create defer demo

---
+++ chapter10/goroutine_demo/cpudemo/runtimedemo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+//获取当前主机的cpu核心数
+func main() {
+
+	// 获取当前系统cpu的核心数
+	numCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+	fmt.Println("cpuNum :", numCPU)
+}
