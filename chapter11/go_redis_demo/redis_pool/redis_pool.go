8380e27086cfa1aa7ac023d267d76b48ed772ec9
tom_glb <goodMorning_glb@atguigu.com>
2020-05-17 22:34:08 +0800

    create redis pool demo

---
+++ chapter11/go_redis_demo/redis_pool/redis_pool.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/garyburd/redigo/redis"
+
+// redis 连接池
+//  1.事先初始化一定数量的连接，放入到链接池中
+//  1.当go需要操作redis时，直接从redis链接池取出连接即可，
+//  1.这样可以节省临时获取redis连接的时间，从而提高效率
+func main() {
+
+}
+
+var pool *redis.Pool
+
+func redisInit(pool *redis.Pool) {
+
+	pool = &redis.Pool{
+		Dial: func() (conn redis.Conn, e error) { // 初始化连接的代码，指定连接那个ip的redis
+			return redis.Dial("tcp", "localhost:6379")
+		},
+		TestOnBorrow:    nil,
+		MaxIdle:         8,   //最大空闲链接数
+		MaxActive:       0,   // 表示和数据库连接的最大连接数， 0表示没有限制
+		IdleTimeout:     100, //最大空闲实践，
+		Wait:            false,
+		MaxConnLifetime: 0,
+	}
+	//get := pool.Get() //从连接池中取出一个连接
+	defer pool.Close() //一旦关闭连接池，就不能从连接池中再取出连接
+
+}
